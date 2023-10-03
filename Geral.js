413abb054bf5532bf5d081dfd5054d9c04170ba8
José Vinícius de Paula Santos <joseviniciusgta@gmail.com>
2023-10-03 23:31:54 +0000

    change to button to confirm habit

--- Geral.js
+++ Geral.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Geral = () => {
     const today = new Date();
@@ -19,14 +20,35 @@ const Geral = () => {
         return dates;
     }
 
+    const [buttonState, setButtonState] = useState('question'); // 'question', 'check', 'close'
+    
+    const handleButtonClick = () => {
+        if (buttonState === 'question') setButtonState('check');
+        else if (buttonState === 'check') setButtonState('close');
+        else setButtonState('question');
+    };
+
+
     const HabitCard = ({ habitName }) => {
+        const buttonColors = {
+            question: 'yellow',
+            check: 'green',
+            close: 'red'
+        };
+
         return (
             <View style={styles.card}>
                 <View style={styles.leftContainer}>
                     <Text style={styles.habitName}>{habitName}</Text>
                     <View style={styles.daysContainer}>
                         {week.slice(0, 4).map((date, index) => (
-                            <View key={index} style={styles.dayBox}>
+                            <View 
+                                key={index} 
+                                style={[
+                                    styles.dayBox, 
+                                    index === 3 ? { borderColor: buttonColors[buttonState] } : {}
+                                ]}
+                            >
                                 <Text style={styles.dayText}>
                                     {daysOfWeek[date.getDay()]}
                                 </Text>
@@ -34,11 +56,12 @@ const Geral = () => {
                         ))}
                     </View>
                 </View>
-                <View style={styles.todayIndicator}>
-                    <Text style={styles.todayText}>
-                        {daysOfWeek[today.getDay()]}
-                    </Text>
-                </View>
+                <TouchableOpacity 
+                    style={[styles.todayButton, { backgroundColor: buttonColors[buttonState] }]}
+                    onPress={handleButtonClick}
+                >
+                    <MaterialIcons name={buttonState} size={24} color="white" />
+                </TouchableOpacity>
             </View>
         );
     };
@@ -139,7 +162,8 @@ const styles = StyleSheet.create({
     },
     todayIndicator: {
         width: 60,
-        marginVertical: 8, // Espaço ao redor do dia atual
+        marginVertical: 8, // Espaço ao redor do dia atual verticalmente
+        marginRight: 10, // Espaço à direita do dia atual
         backgroundColor: 'green',
         justifyContent: 'center',
         alignItems: 'center',
@@ -156,7 +180,14 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         padding: 2,
     },
-    
+    todayButton: {
+        width: 50,
+        height: 50,
+        borderRadius: 25, // Para tornar círculo
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
 
 export default Geral;
