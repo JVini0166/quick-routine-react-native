7c5638bf1ffa05cb27bfd4f3fc9a15f074c6313c
José Vinícius de Paula Santos <joseviniciusgta@gmail.com>
2023-10-24 23:15:47 +0000

    gradient color

--- pages/Login.js
+++ pages/Login.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image, Platform } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Button, Card, TextInput, FAB } from 'react-native-paper';
-
+import { LinearGradient } from 'expo-linear-gradient';
 
 const Login = ({ navigation }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
 
+    
+
+    
     return (
+        <LinearGradient
+        style={styles.gradient}
+        colors={[colors.redLight, colors.blueLight, colors.greenLight]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}>
         <ScrollView style={styles.container}>
-        
             <Card style={styles.card}>
             <View style={styles.logoContainer}>
             <Image source={require('../assets/quickroutine-logo.png')} style={styles.logo} />
@@ -77,25 +84,37 @@ const Login = ({ navigation }) => {
                     Facebook
                 </Button>
             </View>
+            
         </ScrollView>
+        </LinearGradient>
     );
 }
+
+const colors = {
+    redLight: '#FF6B6B',
+    blueLight: '#6BAAFF',
+    greenLight: '#6BFFAA',
+    blackC: '#000000'
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 16,
+        padding: 16,  // Fundo azul claro para todo o contêiner
     },
     
     card: {
         padding: 16,
         marginBottom: 20,
+        backgroundColor: 'white',  // Para garantir que o cartão seja claramente visível contra o fundo
     },
     input: {
         marginBottom: 20,
+        backgroundColor: '#E2E2E2'
     },
     loginButton: {
         marginBottom: 10,
+        backgroundColor: colors.redLight,  // Botão de login vermelho claro
     },
     linkContainer: {
         flexDirection: 'row',
@@ -103,7 +122,7 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
     linkText: {
-        color: '#007BFF',
+        color: colors.black,  // Links de texto verde claro
     },
     socialLoginContainer: {
         flexDirection: 'row',
@@ -114,16 +133,20 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
     logoContainer: {
-        flex: 1, // Isso garante que o container ocupe todo o espaço disponível
-        justifyContent: 'center', // Alinha verticalmente no centro
-        alignItems: 'center',     // Alinha horizontalmente no centro
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
         marginBottom: 40,
     },
     logo: {
         width: 200,
         height: 270,
-        marginBottom: 20,  // Use marginBottom em vez de paddingBottom para dar espaço abaixo da imagem
+        marginBottom: 20,
+    },
+    gradient: {
+        flex: 1
     },
 });
+
 
 export default Login;
