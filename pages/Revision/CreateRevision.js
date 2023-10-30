dd545b1f73d4c872b33d63fbc0dc655c33ebfc6b
José Vinícius de Paula Santos <joseviniciusgta@gmail.com>
2023-10-30 02:57:49 +0000

    create revision logic

--- pages/Revision/CreateRevision.js
+++ pages/Revision/CreateRevision.js
@@ -1,41 +1,95 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { DatePickerInput } from 'react-native-paper-dates';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Dropdown } from 'react-native-element-dropdown';
 
 const CreateRevision = ({ navigation, route }) => {
     const [revisionName, setRevisionName] = useState(''); // <-- State for revision name
     const [description, setDescription] = useState(''); // <-- State for description
+    const [inputStartDate, setInputStartDate] = useState(undefined);
     const [error, setError] = useState(null);
+    const [revisionTemplates, setRevisionTemplates] = useState([]);
 
+    useEffect(() => {
+        const fetchRevisionTemplates = async () => {
+            const storedTemplates = await AsyncStorage.getItem('revisiontemplates');
+            if (storedTemplates) {
+                setRevisionTemplates(JSON.parse(storedTemplates));
+            }
+        };
+    
+        fetchRevisionTemplates();
+    }, []);
+
+    const dropdownData = revisionTemplates.map(template => ({
+        label: template.title,
+        value: template.title
+    }));
+
+    const generateRevisionDate = (startDate, unit, value) => {
+        const date = new Date(startDate);
+    
+        switch (unit) {
+            case 'dias':
+                date.setDate(date.getDate() + parseInt(value));
+                break;
+            case 'meses':
+                date.setMonth(date.getMonth() + parseInt(value));
+                break;
+            case 'anos':
+                date.setFullYear(date.getFullYear() + parseInt(value));
+                break;
+        }
+    
+        return date;
+    };
+
+    const [selectedTemplate, setSelectedTemplate] = useState(null);
+    
+    
     const createRevision = async () => {
         if(!revisionName) {
             setError('Por favor, insira um nome para a revisão.');
             return;
         }
-
+    
+        let revisionDates = [];
+        if (selectedTemplate && inputStartDate) {
+            const template = revisionTemplates.find(t => t.title === selectedTemplate);
+            if (template && template.timeEntries) {
+                for (const entry of template.timeEntries) {
+                    const revisionDate = generateRevisionDate(inputStartDate, entry.unit, entry.value);
+                    revisionDates.push(revisionDate);
+                }
+            }
+        }
+    
         let existingRevisions = await AsyncStorage.getItem('revisions');
         if(!existingRevisions) {
             existingRevisions = [];
         } else {
             existingRevisions = JSON.parse(existingRevisions);
         }
-
+    
         const newId = existingRevisions.length > 0 
             ? (parseInt(existingRevisions[existingRevisions.length - 1].id) + 1).toString()
             : "1";
-
+    
         const newRevision = {
             id: newId,
             revision: revisionName,
             description: description,
+            initDate: inputStartDate,
+            revisionDates: revisionDates,
         };
-
+    
         existingRevisions.push(newRevision);
         await AsyncStorage.setItem('revisions', JSON.stringify(existingRevisions));
-
-        navigation.goBack(); // or other logic
+    
+        navigation.goBack();
     };
 
     return (
@@ -53,13 +107,43 @@ const CreateRevision = ({ navigation, route }) => {
                     value={description}
                     onChangeText={text => setDescription(text)}
                 />
+                <DatePickerInput
+                    locale="pt"
+                    label="Data início"
+                    value={inputStartDate}
+                    onChange={(d) => setInputStartDate(d)}
+                    inputMode="start"
+                    style={styles.input}
+                />
 
                 <Text style={styles.label}>Template</Text>
+                <Dropdown
+                    style={styles.dropdown}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    inputSearchStyle={styles.inputSearchStyle}
+                    iconStyle={styles.iconStyle}
+                    data={dropdownData}
+                    placeholder="Selecione um template"
+                    search
+                    maxHeight={300}
+                    labelField="label"
+                    valueField="value"
+                    searchPlaceholder="Search..."
+                    value={selectedTemplate}
+                    onChange={item => {
+                        setSelectedTemplate(item.value);
+                    }}
+                    // renderLeftIcon={() => (
+                    //     <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
+                    // )}
+                />
+
                 <Button 
                     mode="contained"
                     onPress={() => navigation.navigate('CreateRevisionTemplate')}
                 >
-                    Selecionar
+                    Criar Template de Revisão
                 </Button>
 
                 {error && <Text style={{color: 'red', textAlign: 'center', marginVertical: 10}}>{error}</Text>}
@@ -68,7 +152,7 @@ const CreateRevision = ({ navigation, route }) => {
                     mode="contained"
                     onPress={createRevision} 
                 >
-                    Criar Template
+                    Criar Revisão
                 </Button>
                 
             </View>
--- pages/Revision/CreateRevisionTemplate.js
+++ pages/Revision/CreateRevisionTemplate.js
@@ -5,31 +5,94 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Dropdown } from 'react-native-element-dropdown';
 
 const timeUnitsData = [
-    { label: 'dias', value: 'days' },
-    { label: 'meses', value: 'months' },
-    { label: 'anos', value: 'years' },
+    { label: 'dias', value: 'dias' },
+    { label: 'meses', value: 'meses' },
+    { label: 'anos', value: 'anos' },
 ];
 
-const CreateRevisionTemplate = () => {
+const CreateRevisionTemplate = ({navigation}) => {
     const [selectedTimeUnit, setSelectedTimeUnit] = useState(null);
     const [timeValue, setTimeValue] = useState('');
     const [timeEntries, setTimeEntries] = useState([]);
+    const [templateName, setTemplateName] = useState('');
+    const [error, setError] = useState(null);
 
-    const addTimeEntry = () => {
-        if (timeValue && selectedTimeUnit) {
-            setTimeEntries([...timeEntries, { unit: selectedTimeUnit, value: timeValue }]);
+    const saveTemplate = async () => {
+        setError(null); // Reseta o erro toda vez que a função é chamada
+    
+        if (!templateName.trim() || timeEntries.length === 0) {
+            setError("Por favor, preencha o nome do template e adicione ao menos uma entrada de tempo.");
+            return;
+        }
+    
+        try {
+            const existingTemplatesString = await AsyncStorage.getItem("revisiontemplates");
+            let existingTemplates = existingTemplatesString ? JSON.parse(existingTemplatesString) : [];
+    
+            // Verificar se um template com esse nome já existe
+            if (existingTemplates.some(template => template.title.toLowerCase() === templateName.toLowerCase())) {
+                setError("Já existe um template com esse nome.");
+                return;
+            }
+    
+            existingTemplates.push({
+                title: templateName,
+                timeEntries: timeEntries,
+            });
+    
+            await AsyncStorage.setItem("revisiontemplates", JSON.stringify(existingTemplates));
+            navigation.goBack();
+        } catch (error) {
+            setError("Houve um erro ao salvar o template.");
+            console.error(error);
         }
     };
+    
+    
+    const addTimeEntry = () => {
+
+        setError(null);
+        if (timeValue && selectedTimeUnit) {
+            const exists = timeEntries.some(
+                entry => entry.unit === selectedTimeUnit && entry.value === timeValue
+            );
+            if (exists) {
+                setError("Tempo já foi adicionado.");
+                return;  // Vamos retornar aqui para parar a execução do restante da função.
+            }
+    
+            setTimeEntries(prevEntries => {
+                const newEntries = [...prevEntries, { unit: selectedTimeUnit, value: timeValue }];
+                return newEntries.sort((a, b) => {
+                    if (a.unit === b.unit) {
+                        return Number(a.value) - Number(b.value);
+                    }
+                    return timeUnitsData.findIndex(item => item.value === a.unit) - timeUnitsData.findIndex(item => item.value === b.unit);
+                });
+            });
+        }
+    };
+    
+    
 
     const removeTimeEntry = (index) => {
         const updatedEntries = [...timeEntries];
         updatedEntries.splice(index, 1);
         setTimeEntries(updatedEntries);
     };
+    
 
     return (
         <View style={styles.container}>
+            <Text>Nome do Template de Revisão:</Text>
+                <TextInput 
+                    style={styles.templateNameInput}
+                    value={templateName}
+                    onChangeText={setTemplateName}
+                    placeholder="Digite o nome do template"
+                />
             <View style={styles.row}>
+                
                 <Text>Após</Text>
                 <TextInput 
                     style={styles.input}
@@ -59,10 +122,11 @@ const CreateRevisionTemplate = () => {
                     </Card.Content>
                 </Card>
             ))}
-
+            {error && <Text style={{color: 'red', textAlign: 'center', marginVertical: 10}}>{error}</Text>}
             <Button 
                 mode="contained"
                 style={styles.createButton}
+                onPress={saveTemplate}
             >
                 Criar Template de Revisão
             </Button>
@@ -79,6 +143,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginVertical: 8,
+        justifyContent: 'space-between',
     },
     input: {
         width: 50,
@@ -101,6 +166,15 @@ const styles = StyleSheet.create({
     createButton: {
         marginTop: 24,
     },
+    templateNameInput: {
+        width: '100%',
+        marginBottom: 16,
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 0.5,
+        paddingHorizontal: 10
+    },
+
 });
 
 export default CreateRevisionTemplate;
