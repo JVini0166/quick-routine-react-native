eb1903c763691c04d1412b6ba5f75439eca2d4e3
José Vinícius de Paula Santos <joseviniciusgta@gmail.com>
2023-12-11 17:27:35 +0000

    styles

--- pages/Tasks/EditTask.js
+++ pages/Tasks/EditTask.js
@@ -146,6 +146,7 @@ const styles = StyleSheet.create({
     dateText: {
         textAlign: 'center',
         marginBottom: 10,
+        color: 'white',
     },
     createButton: {
         marginTop: 10,
